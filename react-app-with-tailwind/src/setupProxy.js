1a354fca4b1a23bbe07535bd332064b4c5f07c69
Olalekan Oladimeji <elijahdimeji549@gmail.com>
2023-10-16 01:40:24 +0100

    Stable Version 15/10/2023: Added Server + Client

--- react-app-with-tailwind/src/setupProxy.js
+++ react-app-with-tailwind/src/setupProxy.js
@@ -5,7 +5,7 @@ let baseUrl;
 if (env === "development") {
   baseUrl = "http://localhost:4000"
 } else {
-  baseUrl = "https://trizent-autos-server.vercel.app/"
+  baseUrl = "https://trizent-autos-server.vercel.app"
 }
 module.exports = function(app) {
   app.use(
